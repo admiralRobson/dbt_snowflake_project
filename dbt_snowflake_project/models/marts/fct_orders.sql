29ee8fe527555cfc697205b0516e5c62fea20939
admiralRobson <ivan_creed52@proton.me>
2026-09-04 16:04:41 +0000

    rename hub from hub_product on hub_products and changed stg_products

--- dbt_snowflake_project/models/marts/fct_orders.sql
+++ dbt_snowflake_project/models/marts/fct_orders.sql
@@ -15,6 +15,7 @@ WITH latest_order_sat AS (
 
 SELECT 
     h_ord.order_id,
+    h_ord.order_line_id,
     h_cust.customer_id,
     h_prod.product_id,
     so_sat.order_status,
@@ -23,7 +24,7 @@ SELECT
     so_sat.unit_price,
     (so_sat.quantity * so_sat.unit_price) AS total_amount
 FROM {{ ref('link_order_line') }} lnk
-JOIN {{ ref('hub_order_lines') }} h_ord ON lnk.order_hk = h_ord.order_hk
+JOIN {{ ref('hub_order_lines') }} h_ord ON lnk.order_line_hk = h_ord.order_line_hk
 JOIN {{ ref('hub_customer') }} h_cust ON lnk.customer_hk = h_cust.customer_hk
 JOIN {{ ref('hub_product') }} h_prod ON lnk.product_hk = h_prod.product_hk
 LEFT JOIN latest_order_sat so_sat ON lnk.order_hk = so_sat.order_hk
--- dbt_snowflake_project/models/staging/stg_products.sql
+++ dbt_snowflake_project/models/staging/stg_products.sql
@@ -1,7 +1,7 @@
 select 
     *,
     src.productid as product_id,
-    {{ dbt_utils.generate_surrogate_key(['src.productid'])}} as product_hk,
+    {{ dbt_utils.generate_surrogate_key(['src.productid',''src.''])}} as product_hk,
     {{ dbt_utils.generate_surrogate_key(['src.productid' , 'src.retailprice','src.supplierprice'])}} as product_hash_diff,
     CURRENT_TIMESTAMP() as load_ts,
     'Snowflake' as record_source
